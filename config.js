3e019e226b1e8811ff471f0f79409f6f2d08be96
Julio <julio@ignisgravitas.com>
2019-03-05 02:04:00 -0400

    Start configuration process React Native

---
+++ config.js
@@ -0,0 +1,6 @@
+  // Use the ip associated with your simulator's 
+    // virtual machine (execute ifconfig in a terminal and copy
+    // the result for vboxnetN (N is an integer)
+    export const BASE_URL='http://192.168.58.1:3000'; 
+    export const DEFAULT_EMAIL='test@test.com';
+    export const DEFAULT_PASSWORD='12345678';
