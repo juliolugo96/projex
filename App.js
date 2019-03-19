548ed77ca808327bf37823f767536acd70cdb6ba
Julio <julio@ignisgravitas.com>
2019-03-19 00:10:37 -0400

    Add i18n and multilanguage support

--- App.js
+++ App.js
@@ -1,25 +1,24 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, View} from 'react-native';
-import { Button, Text } from 'native-base';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+import React from 'react';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
+import {StyleSheet} from 'react-native';
+export default class FloatingLabelExample extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Button type="primary">
-          <Text>My simple button</Text></Button>
-      </View>
+      <Container style={styles.container}>
+        <Header />
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
     );
   }
 }
@@ -27,9 +26,7 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#E8F1F2',
   },
   welcome: {
     fontSize: 20,
@@ -41,4 +38,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+}); 
---
+++ i18n/locales/es.js
@@ -0,0 +1,6 @@
+// app/i18n/locales/en.js
+
+export default es = {  
+    greeting: '¡Hola!'
+  };
+  
