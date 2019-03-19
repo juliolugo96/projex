77b98dc1e04c8221b373aef462fffe11b5b6004a
Julio <julio@ignisgravitas.com>
2019-03-19 02:59:25 -0400

    Add basic components to login screen

--- App.js
+++ App.js
@@ -1,41 +1,10 @@
 import React from 'react';
-import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
-import {StyleSheet} from 'react-native';
+import Main from './src/Main';
+
 export default class FloatingLabelExample extends React.Component {
   render() {
     return (
-      <Container style={styles.container}>
-        <Header />
-        <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel last>
-              <Label>Password</Label>
-              <Input />
-            </Item>
-          </Form>
-        </Content>
-      </Container>
+      <Main />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E8F1F2',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-}); 
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoginScreen from './screens/LoginScreen';
+
+export default class Main extends React.Component{
+
+    render(){
+        return (<LoginScreen />);
+    }
+
+}
