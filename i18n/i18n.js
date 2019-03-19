190b2add1582939d38cd8a93de0e44b0699f33ac
Julio <jmanuellugo96@gmail.com>
2019-03-19 00:10:37 -0400

    Add i18n and multilanguage support

---
+++ i18n/i18n.js
@@ -0,0 +1,14 @@
+// app/i18n/i18n.js
+
+import I18n from 'react-native-i18n';
+import en from './locales/en';
+import es from './locales/es';
+
+I18n.fallbacks = true;
+
+I18n.translations = {
+  en,
+  es
+};
+
+export default I18n;
---
+++ i18n/locales/en.js
@@ -0,0 +1,6 @@
+// app/i18n/locales/en.js
+
+export default en = {  
+    greeting: 'Hi!'
+  };
+  
